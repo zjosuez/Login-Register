29f6da9336dc5ed3d0c2d01e7d9084eda9911a84
Josué Teixeira <zjosuez@outlook.com>
2022-09-24 08:05:02 -0300

    Initial Commit

---
+++ src/main/java/com/example/project/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.project.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/project/appuser/AppUserService.java
@@ -0,0 +1,23 @@
+package com.example.project.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG =
+            "Usuário com email %s não encontrado";
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+        .orElseThrow(() ->
+                new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
